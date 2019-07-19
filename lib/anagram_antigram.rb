cbdc0798dd95055fbe9ac8075d83371eb0d94c7e
Alex Siegberg <alex.siegberg@gmail.com>
2019-07-19 09:21:16 -0700

    pass third test, testing if both words contain a vowel

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -6,11 +6,28 @@ class Anagram_Antigram
   def initialize(word1, word2)
     @word_one = word1
     @word_two = word2
+    @word_one_contain_vowel = false
+    @word_two_contain_vowel = false
   end
 
   def anagram
+    @VOWELS = ["a", "e", "i", "o", "u"]
     @word_one_array = @word_one.downcase().split('').sort()
     @word_two_array = @word_two.downcase().split('').sort()
+
+    @VOWELS.each do |vowel|
+      if @word_one_array.include?(vowel)
+        @word_one_contain_vowel = true
+      end
+
+      if @word_two_array.include?(vowel)
+        @word_two_contain_vowel = true
+      end
+    end
+
+    if @word_one_contain_vowel == false || @word_two_contain_vowel == false
+      return "You need to input actual words!"
+    end
 
     if @word_one_array == @word_two_array
       return "#{@word_one} and #{@word_two} are anagrams."
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -3,13 +3,18 @@ require('rspec')
 require('anagram_antigram')
 
 describe('#anagram_antigram') do
-  it('should return true if two words are anagrams') do
+  it('should test if two words are anagrams') do
     test = Anagram_Antigram.new('ruby', 'bury')
     expect(test.anagram()).to(eq("ruby and bury are anagrams."))
   end
 
-  it('should return true if two words are anagrams regardless of case') do
+  it('should test if two words are anagrams regardless of case') do
     test = Anagram_Antigram.new('Ruby', 'Bury')
     expect(test.anagram()).to(eq("Ruby and Bury are anagrams."))
+  end
+
+  it('should test if inputs contain a vowel') do
+    test = Anagram_Antigram.new('ruby', 'btr')
+    expect(test.anagram()).to(eq("You need to input actual words!"))
   end
 end
