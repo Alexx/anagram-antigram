6075ca0e8d2e84849852e4c1a53894cacd75c40e
Alex Siegberg <alex.siegberg@gmail.com>
2019-07-19 08:46:32 -0700

    pass first test testing if two words are an anagram

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -1,13 +1,21 @@
-#!/user/bin/env ruby
-
 require('pry')
 
 class Anagram_Antigram
-  def initialize(word)
-    @word = word
+  attr_reader(:word_one, :word_two)
+
+  def initialize(word1, word2)
+    @word_one = word1
+    @word_two = word2
   end
 
-  def amagram
-    #code
+  def anagram?
+    @word_one_array = @word_one.split('').sort()
+    @word_two_array = @word_two.split('').sort()
+
+    if @word_one_array == @word_two_array
+      return true
+    else
+      return false
+    end
   end
 end
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -3,8 +3,8 @@ require('rspec')
 require('anagram_antigram')
 
 describe('#anagram_antigram') do
-  it('should do something') do
-    test = Anagram_Antigram.new('hi')
-    expect(test.anagram()).to(eq('something'))
+  it('should return true if two words are anagrams') do
+    test = Anagram_Antigram.new('ruby', 'bury')
+    expect(test.anagram?()).to(eq(true))
   end
 end
