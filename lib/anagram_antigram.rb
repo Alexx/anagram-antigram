f410869042313ab7b2292699ede084580d3630c0
Alex Siegberg <alex.siegberg@gmail.com>
2019-07-19 11:49:06 -0700

    refactor anagram method into smaller methods, in progess

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -6,35 +6,40 @@ class Anagram_Antigram
   def initialize(phrase1, phrase2)
     @phrase_one = phrase1
     @phrase_two = phrase2
-    @contain_vowel = true
+    @valid_phrase = true
   end
 
-  def anagram
+  def is_valid_phrase?()
     @VOWELS = ["a", "e", "i", "o", "u"]
-    @phrase_one_array = @phrase_one.downcase().split(' ')
-    @phrase_two_array = @phrase_two.downcase().split(' ')
+    @check_phrase_one = @phrase_one.downcase().split(' ')
+    @check_phrase_two = @phrase_two.downcase().split(' ')
 
     @phrase_one_array.each do |word|
       if !word.split('').any? { |vowel| @VOWELS.include?(vowel) }
-        @contain_vowel = false
+        @valid_phrase = false
       end
     end
 
     @phrase_two_array.each do |word|
       if !word.split('').any? { |vowel| @VOWELS.include?(vowel) }
-        @contain_vowel = false
+        @valid_phrase = false
       end
     end
 
-    if @contain_vowel == false
-      return "You need to input actual words!"
+    if @valid_phrase == false
+      return false
     end
 
-    #Join words in phrase together in alphabetical order and delete all empty spaces
-    @phrase_one_array = @phrase_one_array.join('').split('').sort().reject { |e| e.empty? }
-    @phrase_two_array = @phrase_two_array.join('').split('').sort().reject { |e| e.empty? }
+  end
 
-    if @phrase_one_array == @phrase_two_array
+  def anagram
+    #Force words in phrase to lowercase, in alphabetical order and delete all empty spaces
+    @phrase_one_array = @phrase_one.downcase().split('').sort()
+    @phrase_two_array = @phrase_two.downcase().split('').sort()
+    p is_valid_phrase?()
+    if is_valid_phrase?()
+      return "You need to input actual words!"
+    elsif @phrase_one_array == @phrase_two_array
       return "#{@phrase_one} and #{@phrase_two} are anagrams."
     elsif !@phrase_one_array.any? {|letter| @phrase_two_array.include?(letter)}
       return "#{@phrase_one} and #{@phrase_two} are antigrams."
